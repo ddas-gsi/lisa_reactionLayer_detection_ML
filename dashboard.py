4854d6ab6918e94e84570f283faad59489c03d99
ddas-gsi <debajyotidas2017@gmail.com>
2025-04-19 10:26:25 +0200

    updaed range for x-axis

--- dashboard.py
+++ dashboard.py
@@ -43,14 +43,19 @@ while True:
                     col1, col2 = st.columns(2)
 
                     with col1:
-                        fig_l0 = px.histogram(df, x="x1", color="Layer", title="dE_L0 Distribution", nbins=30)
+                        fig_l0 = px.histogram(df, x="x1", color="Layer", title="dE_L0 Distribution", nbins=100)
+                        # fig_l0 = px.histogram(df, x="x1", color="Layer", title="dE_L0 Distribution")
+                        fig_l0.update_layout(xaxis_range=[500, 1500])
                         st.plotly_chart(fig_l0, use_container_width=True, key=f"hist_l0_{update_counter}")
 
                     with col2:
-                        fig_l1 = px.histogram(df, x="x2", color="Layer", title="dE_L1 Distribution", nbins=30)
+                        fig_l1 = px.histogram(df, x="x2", color="Layer", title="dE_L1 Distribution", nbins=100)
+                        # fig_l1 = px.histogram(df, x="x2", color="Layer", title="dE_L1 Distribution")
+                        fig_l1.update_layout(xaxis_range=[500, 1500])
                         st.plotly_chart(fig_l1, use_container_width=True, key=f"hist_l1_{update_counter}")
 
-                    fig_tot = px.histogram(df, x="x3", color="Layer", title="dE_Tot Distribution", nbins=30)
+                    fig_tot = px.histogram(df, x="x3", color="Layer", title="dE_Tot Distribution", nbins=100, range_x=[500,2500])
+                    # fig_tot = px.histogram(df, x="x3", color="Layer", title="dE_Tot Distribution")
                     st.plotly_chart(fig_tot, use_container_width=True, key=f"hist_tot_{update_counter}")
 
                 update_counter += 1  # Increment key version
--- randomEnergy.py
+++ randomEnergy.py
@@ -9,13 +9,15 @@ BACKEND_URL = "http://localhost:8000/predict_batch"
 def generate_random_sample():
     dE_L0 = random.uniform(910.0, 1025.0)    # random.uniform(lowerLimit, upperLimit)
     dE_L1 = random.uniform(925.0, 1035.0)    # random.uniform(lowerLimit, upperLimit)
+    # dE_L0 = random.uniform(500.0, 1500.0)    # random.uniform(lowerLimit, upperLimit)
+    # dE_L1 = random.uniform(500.0, 1500.0)    # random.uniform(lowerLimit, upperLimit)
     dE_Tot = dE_L0 + dE_L1
     return {"x1": dE_L0, "x2": dE_L1, "x3": dE_Tot}
 
 count = 1
 
 while True:
-    batch = [generate_random_sample() for _ in range(100)]
+    batch = [generate_random_sample() for _ in range(1000)]
     try:
         response = requests.post(BACKEND_URL, json={"data": batch})
         result = response.json()
